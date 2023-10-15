02f3f49eb25b489d3f9c39a5c9447ca35dbe92f0
amirfazel <amirfazel45@gmail.com>
2023-10-15 20:03:55 +0330

    added db queries to  first api

--- base.py
+++ base.py
@@ -2,5 +2,6 @@ BASE_DATA = {
     "ACCESS_KEY": "91ba6d98-079f-4601-939f-30ef872d084f",
     "SECRET_KEY": "277d66a89d2893a8f5e8a5b0dc50d0b023735aad39c32114d81d937b01a4f969",
     "ARVAN_CLOUD_ENDPOINT": "https://s3.ir-thr-at1.arvanstorage.com/",
-    "BUCKET_NAME": "ccass1"
+    "BUCKET_NAME": "ccass1",
+    "ATLAS_PASSWORD": "CCAss1_DBaas"
 }
